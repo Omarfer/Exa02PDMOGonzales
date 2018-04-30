633d963577d36d1b17fa28d3a2d81da5bdc1ad69
Omarfer <omarfer007@gmail.com>
2018-04-30 18:38:39 -0500

    Initial commit

---
+++ app/src/main/java/com/omarfer/exa02_pdm_ogonzales/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.omarfer.exa02_pdm_ogonzales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
