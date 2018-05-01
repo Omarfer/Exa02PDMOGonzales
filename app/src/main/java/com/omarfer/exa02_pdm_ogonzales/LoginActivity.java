281807a9f8e69dc5b1d51ad82356a2515f300331
Omarfer <omarfer007@gmail.com>
2018-05-01 07:48:26 -0500

    connections

--- app/src/main/java/com/omarfer/exa02_pdm_ogonzales/LoginActivity.java
+++ app/src/main/java/com/omarfer/exa02_pdm_ogonzales/LoginActivity.java
@@ -1,13 +1,82 @@
 package com.omarfer.exa02_pdm_ogonzales;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText usuario, clave;
+    private Button logear;
+    private FirebaseAuth auth;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() == null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            isDestroyed();
+        }
+
+        setContentView(R.layout.activity_login);
+        usuario = (EditText) findViewById(R.id.username);
+        clave = (EditText) findViewById(R.id.password);
+        logear = (Button) findViewById(R.id.login);
+
+
     }
+    public void Ingresar(View view) {
+        String user = usuario.getText().toString();
+        final String pass = clave.getText().toString();
+
+        if (TextUtils.isEmpty(user)) {
+            Toast.makeText(getApplicationContext(), "Ingresar Usuario!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(user, pass)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            if (pass.length() < 6) {
+                                Toast.makeText(LoginActivity.this, getString(R.string.minimum_password), Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+    }
+
 }
