ff4acf1fbb73c73f40d9a061a8832cc57f6a1599
Omarfer <omarfer007@gmail.com>
2018-05-01 03:09:26 -0500

    basics

--- app/src/main/java/com/omarfer/exa02_pdm_ogonzales/LoginActivity.java
+++ app/src/main/java/com/omarfer/exa02_pdm_ogonzales/LoginActivity.java
@@ -3,11 +3,11 @@ package com.omarfer.exa02_pdm_ogonzales;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_login);
     }
 }
